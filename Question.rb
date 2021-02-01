1fa027b47b2641e2df07735d03d7184777ef1b4d
Rene Wang <rene.wang1@hotmail.com>
2021-02-01 21:17:57 +0000

    implemented question class

--- Question.rb
+++ Question.rb
@@ -4,7 +4,17 @@ class Question
     @number2 = nil
   end
 
-  def new_question
-    
+  def new_question(turn)
+    @number1 = rand(10)
+    @number2 = rand(10)
+    if turn == 1
+      puts "Player1: What does #{@number1} plus #{@number2} equal?"
+    else 
+      puts "Player2: What does #{@number1} plus #{@number2} equal?"
+    end
+  end
+
+  def check_answer(answer)
+    if answer == @number1 + @number2
   end
 end
