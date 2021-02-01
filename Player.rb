19aeb474f8f6a628a89d21d0cd89d03d17313423
Rene Wang <rene.wang1@hotmail.com>
2021-02-01 21:28:07 +0000

    change reduce_life function to not take parameters

--- Player.rb
+++ Player.rb
@@ -1,15 +1,11 @@
 class Player
-  attr_reader :player1_lives, :player2_lives
+  attr_reader :player_lives
 
   def initialize
     @player_lives = 3
   end
 
-  def reduce_life(player)
-    if player == 1
-      @player1_lives -= 1
-    elsif player == 2
-      @player2_lives -= 1
-    end
+  def reduce_life
+    @player_lives -= 1
   end
 end
