0ec1b1ae3f4e62b0bb22bd56da819f6f634b14b6
Rene Wang <rene.wang1@hotmail.com>
2021-02-01 18:47:23 +0000

    added initialize to lives and defined reduce_life function

--- Lives.rb
+++ Lives.rb
@@ -1,3 +1,16 @@
 class Lives
-  
+  attr_reader :player1_lives, :player2_lives
+
+  def initialize
+    @player1_lives = 3
+    @player2_lives = 3
+  end
+
+  def reduce_life(player)
+    if player == 1
+      @player1_lives -= 1
+    elsif player == 2
+      @player2_lives -= 1
+    end
+  end
 end
