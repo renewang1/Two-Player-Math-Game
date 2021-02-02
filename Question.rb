5909765ae374a3d804980abfbbf04e1dd55c3aaa
Rene Wang <rene.wang1@hotmail.com>
2021-02-02 02:06:48 +0000

    add string to indicate new turn

--- Question.rb
+++ Question.rb
@@ -6,6 +6,7 @@ class Question
   end
 
   def new_question
+    puts "----- NEW TURN -----"
     @number1 = rand(10)
     @number2 = rand(10)
     if @turn == 1
@@ -20,9 +21,11 @@ class Question
   def check_answer(answer)
     if answer == @number1 + @number2
       puts "YES! You are correct."
+      true
       # update_turn
     else
       puts "Seriously? No!"
+      false
       # self.lower_score(turn)
     end
   end
