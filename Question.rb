2ab27d0e7bac03df1e0a126731f1c038e9dbb231
Rene Wang <rene.wang1@hotmail.com>
2021-02-02 01:47:06 +0000

    missing end in check_answer method

--- Question.rb
+++ Question.rb
@@ -1,20 +1,29 @@
 class Question
-  def initialize
+  def initialize(turn)
     @number1 = nil
     @number2 = nil
+    @turn = turn
   end
 
-  def new_question(turn)
+  def new_question
     @number1 = rand(10)
     @number2 = rand(10)
-    if turn == 1
+    if @turn == 1
       puts "Player1: What does #{@number1} plus #{@number2} equal?"
-    else 
+    elsif @turn == 2
       puts "Player2: What does #{@number1} plus #{@number2} equal?"
     end
+    answer = gets.chomp.to_i
+    check_answer(answer)
   end
 
   def check_answer(answer)
     if answer == @number1 + @number2
+      puts "YES! You are correct."
+      # update_turn
+    else
+      puts "Seriously? No!"
+      # self.lower_score(turn)
+    end
   end
 end
